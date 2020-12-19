71ed7f36dd3373af7d647f802309a743114da49e
Takuan2740 <5235rwe4kh@privaterelay.appleid.com>
2020-12-19 14:35:05 +0900

    ログイン機能およびセキュリティ機能を実装しました。

--- src/main/java/mrs/domain/service/user/ReservationUserDetails.java
+++ src/main/java/mrs/domain/service/user/ReservationUserDetails.java
@@ -1,5 +1,58 @@
 package mrs.domain.service.user;
 
-public class ReservationUserDetails {
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import mrs.domain.model.User;
+
+//springsecurityで定義されているUserDetailsを実装する
+public class ReservationUserDetails implements UserDetails {
+
+	private final User user;
+
+	public ReservationUserDetails(User user) {
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList("ROLE" + this.user.getRoleName().name());
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getUserId();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }

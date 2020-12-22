fcf6c7bea4d1d37af1fa3e94a55c7a01f89454a3
mshunji <iijava41@gmail.com>
2020-12-22 22:51:05 +0900

    ログイン処理の完了。

--- src/main/java/mrs/domain/repository/user/UserRepository.java
+++ src/main/java/mrs/domain/repository/user/UserRepository.java
@@ -1,23 +1,9 @@
 package mrs.domain.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-=======
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
->>>>>>> refs/remotes/origin/main
 
 import mrs.domain.model.User;
 
-<<<<<<< HEAD
-public interface UserRepository extends JpaRepository<User, String>{
-=======
-@Repository
->>>>>>> refs/remotes/origin/main
-
-<<<<<<< HEAD
-=======
-@Transactional
 public interface UserRepository extends JpaRepository<User, String> {
->>>>>>> refs/remotes/origin/main
+
 }

a8972b75e29fce5e86620abce202c394e86cfc3c
Takuan2740 <5235rwe4kh@privaterelay.appleid.com>
2021-02-03 11:36:04 +0900

    schema.sqlの修正。

--- src/main/java/mrs/app/reservation/ReservationsController.java
+++ src/main/java/mrs/app/reservation/ReservationsController.java
@@ -64,8 +64,7 @@ public class ReservationsController {
 
 	static Map<UUID, Timer> mapIdForTimer = new HashMap<>();
 
-	//予約確認・予約一覧画面
-	//予約可能かの確認作業はreserve()にて
+	//予約確認・予約一覧画面に遷移するメソッド
 	@RequestMapping(method = RequestMethod.GET)
 	String reserveForm(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
 			@PathVariable("roomId") Integer roomId, Model model) {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,8 +3,17 @@ DROP TABLE IF EXISTS reservable_room CASCADE ;
 DROP TABLE IF EXISTS reservation CASCADE ;
 DROP TABLE IF EXISTS usr CASCADE ;
 
+
+CREATE SEQUENCE roomID_seq;
+SELECT setval('roomID_seq',1,false);
+ALTER SEQUENCE roomID_seq RESTART WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE reservationID_seq;
+SELECT setval('reservationID_seq',1,false);
+ALTER SEQUENCE reservationID_seq RESTART WITH 1 INCREMENT BY 1;
+
 CREATE TABLE IF NOT EXISTS meeting_room (
-  room_id SERIAL NOT NULL,
+  room_id Integer DEFAULT nextval('roomID_seq') NOT NULL,
   room_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (room_id)
 );
@@ -16,7 +25,7 @@ CREATE TABLE IF NOT EXISTS reservable_room (
 );
 
 CREATE TABLE IF NOT EXISTS reservation (
-  reservation_id SERIAL NOT NULL,
+  reservation_id Integer DEFAULT nextval('reservationID_seq') NOT NULL,
   end_time TIME NOT NULL,
   start_time TIME NOT NULL,
   reserved_date DATE NOT NULL,
